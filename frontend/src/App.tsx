6035d4f1ab4fafd821eff303d60cbba06240fbf6
Daniel Kapkov <Gedztodu@gmail.com>
2022-10-28 22:12:01 +0300

    Init cra

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./UI/Header/Header";
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ frontend/src/UI/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">a</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
