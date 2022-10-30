f7691c364ebf86a9d7057ac584572711658fb762
Daniel Kapkov <Gedztodu@gmail.com>
2022-10-30 16:41:50 +0200

    Add db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: "postgres",
+    url: process.env["DATABASE_URL"],
+    autoLoadEntities: true,
+    synchronize: true
+  }), ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
--- frontend/src/UI/Header/Header.tsx
+++ frontend/src/UI/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     return (
         <Router>
             <header>
-                <nav className={styles.header}>
+                <nav className={styles.header}>at
                     <ul className={styles.menu}>
                         <li>
                             <Link to="/" className={styles.link}>News</Link>
