f05cb736e1843d458322c0ccd281be04762b4def
Daniel Kapkov <Gedztodu@gmail.com>
2022-11-30 16:52:09 +0200

    Shut down remote cluster. Delete config

--- backend/config/database.config.example.js
+++
@@ -1,1 +0,0 @@
-export const CONNECTION_URL_DEPLOY = "mongodb+srv://salienteNotes:<password>@cluster0.t2djgvm.mongodb.net/?retryWrites=true&w=majority"
--- backend/config/database.config.js
+++
@@ -1,1 +0,0 @@
-export const CONNECTION_URL = "mongodb+srv://salienteNotes:ZS2Tlgnpv3ZQNqtz@cluster0.t2djgvm.mongodb.net/?retryWrites=true&w=majority"
