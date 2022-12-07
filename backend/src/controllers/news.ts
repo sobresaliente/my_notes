cffc7dbadc28ae38fb80b324e4c3e882c0f9bf5e
Daniel Kapkov <Gedztodu@gmail.com>
2022-12-07 14:55:24 +0200

    Refactor files using prettier

--- backend/src/controllers/news.ts
+++ backend/src/controllers/news.ts
@@ -1,45 +1,47 @@
-import NewsPost from '../news/newsModels.js'
-import mongoose from "mongoose";
+import NewsPost from '../news/newsModels.js';
+import mongoose from 'mongoose';
 
 export const getNews = async (req: any, res: any) => {
-    try {
-        const news = await NewsPost.find();
-        res.status(200).json(news);
-    } catch(error) {
-        res.status(404).json({ message: error })
-    }
-}
+  try {
+    const news = await NewsPost.find();
+    res.status(200).json(news);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
+};
 
 export const createNews = async (req: any, res: any) => {
-    const newsPost = req.body;
+  const newsPost = req.body;
 
-    const newNewsPost = new NewsPost(newsPost);
+  const newNewsPost = new NewsPost(newsPost);
 
-    try {
-        await newNewsPost.save();
+  try {
+    await newNewsPost.save();
 
-        res.status(201).json(newNewsPost)
-    } catch (error) {
-        res.status(409).json({ message: error })
-    }
-}
+    res.status(201).json(newNewsPost);
+  } catch (error) {
+    res.status(409).json({ message: error });
+  }
+};
 
 export const updateNews = async (req: any, res: any) => {
-    const {id: _id} = req.params;
-    const newNewsPost = req.body;
-    if (!mongoose.Types.ObjectId.isValid(_id)) {
-        return res.status(404).send("Not found");
-    }
-    const updatedNewsPost = await NewsPost.findByIdAndUpdate(_id, {...newNewsPost, _id}, {new: true})
-    res.json(updatedNewsPost);
-}
+  const { id: _id } = req.params;
+  const newNewsPost = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send('Not found');
+  }
+
+  const updatedNewsPost = await NewsPost.findByIdAndUpdate(_id, { ...newNewsPost, _id }, { new: true });
+  res.json(updatedNewsPost);
+};
 
 export const deleteNews = async (req: any, res: any) => {
-    const {id: _id} = req.params;
-    if (!mongoose.Types.ObjectId.isValid(_id)) {
-        return res.status(404).send("Not found");
-    }
+  const { id: _id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send('Not found');
+  }
 
-    await NewsPost.findByIdAndRemove(_id);
-    res.json({message: "News post deleted"});
-}
+  await NewsPost.findByIdAndRemove(_id);
+  res.json({ message: 'News post deleted' });
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,18 +9,14 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb' }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
 app.use('/news', newsRoutes);
 
 const PORT = 5000;
 const db = 'mongodb://localhost/';
 
 try {
-  // Connect to the MongoDB cluster
-  mongoose.connect(
-    db,
-
-    () => app.listen(PORT, () => console.log('Server started')),
-  );
+  mongoose.connect(db, () => app.listen(PORT, () => console.log('Server started')));
 } catch (e) {
   console.log('could not connect');
 }
