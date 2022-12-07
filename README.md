5bf037476773418e0da9425ba92661f2e72edf30
Daniel Kapkov <Gedztodu@gmail.com>
2022-12-07 18:33:39 +0200

    News mark up change, implement tailwind, add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# My_notes application
+
+## Status: WIP
+
+
+## Plans:
+
+* CRUD of notes
+* CRUD of projects
+* Implement simple admin panel
--- frontend/src/components/NewsPost/NewsPost.jsx
+++ frontend/src/components/NewsPost/NewsPost.jsx
@@ -9,8 +9,8 @@ const NewsPost = ({newsPost, setNewsId}) => {
         dispatch(deleteNews(newsPost._id));
     }
     return (
-        <div>
-            <h2>
+        <div className="ml-40 mt-28 border-amber-50 border-2 w-1/3 h-1/4 p-8">
+            <h2 className="text-xl font-medium accent-indigo-50">
                 {newsPost.title}
             </h2>
             <div>
@@ -20,8 +20,8 @@ const NewsPost = ({newsPost, setNewsId}) => {
                 {newsPost.tags}
             </div>
             <span>{newsPost.createdAt.slice(0, 10)}</span>
-            <button onClick={onDelete}>Delete news</button>
-            <button onClick={() => setNewsId(newsPost._id)}>Edit news</button>
+            <button onClick={onDelete} className="ml-20">Delete news</button>
+            <button onClick={() => setNewsId(newsPost._id)} className="ml-5">Edit news</button>
         </div>
     );
 };
